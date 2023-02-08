f0f4925520ac405f0f6d37710f095f8e051cee2e
kurbatov <kurbatxx@gmail.com>
2023-02-08 17:54:52 +0600

    initial state

---
+++ lib/api/api.dart
@@ -0,0 +1,5 @@
+class Api {
+  Future<bool> login(String login, String password) async {
+    return true;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,56 @@
+import 'package:card_crm/providers/secure_storage_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
   final wsUrl = Uri.parse('ws://127.0.0.1:3333/ws');
   var channel = WebSocketChannel.connect(wsUrl);
 
-  runApp(MainApp(channel: channel));
+  runApp(
+    ProviderScope(
+      child: MaterialApp(
+        theme: ThemeData(useMaterial3: true),
+        darkTheme: ThemeData.dark(),
+        home: const App(),
+      ),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  final WebSocketChannel channel;
-  const MainApp({super.key, required this.channel});
+class App extends ConsumerWidget {
+  const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(onPressed: () async {
-          channel.sink.add('recheived!');
-          //channel.sink.close(status.goingAway);
-        }),
-        body: Center(
-          child: StreamBuilder(
-            stream: channel.stream.asBroadcastStream(),
-            builder: (
-              BuildContext context,
-              AsyncSnapshot<dynamic> snapshot,
-            ) {
-              if (snapshot.hasData) {
-                return Text(snapshot.toString());
-              }
-              return const Text("Ничего");
-            },
-          ),
-        ),
-        //child: Text('Hello World!'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(loginProvider, (_, next) {
+      print(next.value);
+    });
+
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
       ),
+      // floatingActionButton: FloatingActionButton(onPressed: () async {
+      //   channel.sink.add('recheived!');
+      //   //channel.sink.close(status.goingAway);
+      // }),
+      // body: Center(
+      //   child: StreamBuilder(
+      //     stream: channel.stream.asBroadcastStream(),
+      //     builder: (
+      //       BuildContext context,
+      //       AsyncSnapshot<dynamic> snapshot,
+      //     ) {
+      //       if (snapshot.hasData) {
+      //         return Text(snapshot.toString());
+      //       }
+      //       return const Text("Ничего");
+      //     },
+      //   ),
+      // ),
+      // //child: Text('Hello World!'),
     );
   }
 }
